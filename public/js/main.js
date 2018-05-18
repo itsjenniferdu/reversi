5dc0b15661fe13e873c693912733700c75b666c4
Jenn <itsjenniferdu@gmail.com>
2018-05-17 19:57:03 -0700

    socket library with lobby names

--- public/js/main.js
+++ public/js/main.js
@@ -7,20 +7,27 @@ function getURLParameters(whichParam)
 	var pageURL = window.location.search.substring(1);
 	var pageURLVariables = pageURL.split('&');
 	for(var i = 0; i < pageURLVariables.length; i++){
-		var parameterName = pageURLVariables[i].split(‘=’);
-		if(parameterName[0] == whichParam){	
-			return parameterName[1];
+	var parameterName = pageURLVariables[i].split('=');
+	if(parameterName[0] == whichParam){
+		return parameterName[1];
 		}
 	}
+
 }
 
-var username = getURLParameters(‘username’);
-if(‘undefined’ == typeof username || !username){
-	username = ‘Anonymous_’+Math.random();
+var username = getURLParameters('username');
+if('undefined' == typeof username || !username){
+	username = 'Anonymous_'+Math.random();
 }
+$('#messages').append('<h4>'+username+'</h4>');
+ 
+/* Connect to the socket server */ 
+ 
+var socket = io.connect();
 
-$(‘#messages’).append(‘<h4>’+username+’</h4>’);
-
+socket.on('log',function(array){
+	console.log.apply(console,array);
+});
 
 
 
