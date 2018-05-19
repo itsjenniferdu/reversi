3078bdd70d98ef878aba9c851773bbd95db3fc3d
Jenn <itsjenniferdu@gmail.com>
2018-05-19 00:12:38 -0400

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -19,8 +19,10 @@ var username = getURLParameters('username');
 if('undefined' == typeof username || !username){
 	username = 'Anonymous_'+Math.random();
 }
-$('#messages').append('<h4>'+username+'</h4>');
- 
+
+var chat_room = 'One_Room'; 
+
+
 /* Connect to the socket server */ 
  
 var socket = io.connect();
@@ -29,5 +31,21 @@ socket.on('log',function(array){
 	console.log.apply(console,array);
 });
 
+socket.on('join_room_response',function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	$('#messages').append('<p>New user joined the room:'+payload.username+'</p>');
 
+});
+
+$(function(){
+	var payload = {};
+	payload.room = chat_room;
+	payload.username = username;
+
+	console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
+	socket.emit('join_room',payload);
+});
 
