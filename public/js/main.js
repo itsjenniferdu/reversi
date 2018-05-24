5ac3a82fa101db03bd328327b56837f47719699c
itsjenniferdu <38433784+itsjenniferdu@users.noreply.github.com>
2018-05-23 19:29:08 -0700

    Update main.js
    copy and paste from atom

--- public/js/main.js
+++ public/js/main.js
@@ -7,38 +7,53 @@ function getURLParameters(whichParam)
 	var pageURL = window.location.search.substring(1);
 	var pageURLVariables = pageURL.split('&');
 	for(var i = 0; i < pageURLVariables.length; i++){
-	var parameterName = pageURLVariables[i].split('=');
-	if(parameterName[0] == whichParam){
-		return parameterName[1];
+		var parameterName = pageURLVariables[i].split('=');
+		if(parameterName[0] == whichParam){
+			return parameterName[1];
 		}
 	}
 
 }
-
 var username = getURLParameters('username');
 if('undefined' == typeof username || !username){
 	username = 'Anonymous_'+Math.random();
 }
+$('#messages').append('<h4>'+username+'</h4>');
 
-var chat_room = 'One_Room'; 
+var chat_room = 'One_Room';
 
-
-/* Connect to the socket server */ 
- 
+/* Connect to the socket server */
 var socket = io.connect();
 
 socket.on('log',function(array){
 	console.log.apply(console,array);
 });
+/* */
 
 socket.on('join_room_response',function(payload){
 	if(payload.result == 'fail'){
 		alert(payload.message);
 		return;
 	}
-	$('#messages').append('<p>New user joined the room:'+payload.username+'</p>');
-
+	$('#messages').append('<p>New user joined the room: '+payload.username+'</p>');
 });
+
+socket.on('send_message_response',function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	$('#messages').append('<p><b>'+payload.username+' says:</b> '+payload.message+'</p>');
+});
+
+function send_message (){
+	var payload = {};
+	payload.room = chat_room;
+	payload.username = username;
+	payload.message = $('#send_message_holder').val();
+	console.log('*** Client Log Message: \'send_message\' payload: '+JSON.stringify(payload));
+	socket.emit('send_message',payload);
+}
 
 $(function(){
 	var payload = {};
@@ -48,4 +63,3 @@ $(function(){
 	console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
 	socket.emit('join_room',payload);
 });
-
